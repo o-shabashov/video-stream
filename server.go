1a5e2d812e0e96bf02fd267f747e5e52f823c5ca
Oleg Shabashov <oleg.shabashov@gmail.com>
2021-03-16 20:03:08 +0700

    Respond with simple MP4 file with the correct MIME type

--- server.go
+++ server.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-    "html"
-    "io"
-    "log"
-    "net/http"
+	"html"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
-	runServer()
-}
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("%q %q", html.EscapeString(request.Method), html.EscapeString(request.URL.Path))
 
-func runServer() {
-    http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-        log.Printf("%q %q", html.EscapeString(request.Method), html.EscapeString(request.URL.Path))
-        _, _ = io.WriteString(writer, html.EscapeString(request.URL.Path))
-    })
+		movieFile, err := os.Open("D:\\Видео\\Game Records\\Clips\\GWF #1.mp4")
+		if err != nil {
+			panic(err)
+		}
+		defer movieFile.Close()
 
-    log.Fatal(http.ListenAndServe(":8899", nil))
+		fileHeader := make([]byte, 512)
+		movieFile.Read(fileHeader)
+		contentType := http.DetectContentType(fileHeader)
+
+		movieFileStat, err := movieFile.Stat()
+		if err != nil {
+			panic(err)
+		}
+
+		writer.Header().Set("Content-Type", contentType)
+		writer.Header().Set("Content-Length", strconv.Itoa(int(movieFileStat.Size())))
+
+		movieFile.Seek(0, 0)
+
+		_, err = io.Copy(writer, movieFile)
+		if err != nil {
+			panic(err)
+		}
+		return
+	})
+
+	log.Fatal(http.ListenAndServe(":8899", nil))
 }
