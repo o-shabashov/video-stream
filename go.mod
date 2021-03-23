b95d4665b7b42f4b31d5190d52c05d0ce384d2ae
Oleg Shabashov <oleg.shabashov@gmail.com>
2021-03-23 14:03:59 +0700

    Respond by chunks by 100Kb each, calculate uploading duration for each chunk

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/oshabashov/video-stream
 
 go 1.16
+
+require github.com/mitchellh/gox v1.0.1 // indirect
--- server.go
+++ server.go
@@ -1,44 +1,103 @@
 package main
 
 import (
+	"bufio"
 	"html"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
+)
+
+const (
+	sampleVideo       = "sample.mp4"
+	chunkSize   int64 = 100 * 1024
+)
+
+var (
+	part  []byte
+	count int
 )
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		log.Printf("%q %q", html.EscapeString(request.Method), html.EscapeString(request.URL.Path))
 
-		movieFile, err := os.Open("D:\\Видео\\Game Records\\Clips\\GWF #1.mp4")
-		if err != nil {
-			panic(err)
-		}
-		defer movieFile.Close()
+		file, err := os.Open(sampleVideo)
+		check(err)
+		defer file.Close()
 
 		fileHeader := make([]byte, 512)
-		movieFile.Read(fileHeader)
+		file.Read(fileHeader)
 		contentType := http.DetectContentType(fileHeader)
+		file.Seek(0, 0)
 
-		movieFileStat, err := movieFile.Stat()
-		if err != nil {
-			panic(err)
-		}
+		movieFileStat, err := file.Stat()
+		check(err)
 
 		writer.Header().Set("Content-Type", contentType)
 		writer.Header().Set("Content-Length", strconv.Itoa(int(movieFileStat.Size())))
 
-		movieFile.Seek(0, 0)
+		//channel := make(chan int64)
 
-		_, err = io.Copy(writer, movieFile)
-		if err != nil {
-			panic(err)
+		reader := bufio.NewReader(file)
+		//buffer := bytes.NewBuffer(make([]byte, 0))
+		part = make([]byte, chunkSize)
+
+		for {
+			start := time.Now()
+			written, err := io.CopyN(writer, reader, chunkSize)
+			if err != nil {
+				break
+			}
+			duration := time.Since(start)
+			log.Printf("Written: %v", written)
+			log.Printf("Duration: %v", duration)
 		}
+
+		if err != io.EOF && err != nil {
+			log.Fatal("Error Reading ", sampleVideo, ": ", err)
+		} else {
+			err = nil
+		}
+
+		//for i := range channel {
+		//	log.Println(i)
+		//}
+
 		return
 	})
 
 	log.Fatal(http.ListenAndServe(":8899", nil))
+}
+
+func copyFile(writer http.ResponseWriter, file *os.File) {
+	fileStat, err := file.Stat()
+	check(err)
+	var i int64
+
+	for i = 0; i < fileStat.Size(); i += 100 * 1024 {
+		start := time.Now()
+		_, err := io.CopyN(writer, file, 100*1024)
+		check(err)
+		file.Seek(i, 1)
+		duration := time.Since(start)
+
+		log.Println(duration)
+
+		//channel <- duration.Milliseconds()
+	}
+
+	//close(channel)
+}
+
+func check(e error) {
+	if e == io.EOF {
+		return
+	}
+	if e != nil {
+		panic(e)
+	}
 }
